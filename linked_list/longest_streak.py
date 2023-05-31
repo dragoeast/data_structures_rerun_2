7d3680a4b0b413ec3f3aa67046bf42f650a2a632
Krisztian Markella <chris.krisztian.markella@gmail.com>
2023-05-31 10:32:52 -0500

    longest_streak implemented

---
+++ linked_list/longest_streak.py
@@ -0,0 +1,30 @@
+from create_linked_list import create_linked_list
+
+
+def longest_streak(head):
+    """Return the longes consecutive streak of the same value.
+    >>> head = create_linked_list(values=[2, 3, 3, 7, 7, 7, 1])
+    >>> longest_streak(head)
+    3
+    >>> longest_streak(head=None)
+    0
+    >>> longest_streak(head=create_linked_list(values=[11]))
+    1
+    """
+    max_streak = 0
+    current_streak = 0
+    prev_val = None
+    curr = head
+    while curr is not None:
+        if curr.val == prev_val:
+            current_streak += 1
+        else:
+            current_streak = 1
+
+        if current_streak > max_streak:
+            max_streak = current_streak
+        
+        prev_val = curr.val
+        curr = curr.next
+
+    return max_streak
