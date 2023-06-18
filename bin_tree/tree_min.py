3702bd55b01b5387e7bdd1636fe19449a72ffbc2
Krisztian Markella <chris.krisztian.markella@gmail.com>
2023-06-17 21:33:08 -0500

    iterative DFS implemented

--- bin_tree/tree_min.py
+++ bin_tree/tree_min.py
@@ -1,6 +1,31 @@
 from node import Node
 
 
+def tree_min_iter(root):
+    """Return the minimum valueof the binary tree.
+    BFS iterative version.
+    >>> tree_min_iter(root=None) == float('inf')
+    True
+    >>> bin_tree = Node(10, Node(12), Node(5))
+    >>> tree_min_iter(root=bin_tree)
+    5
+    """
+    if root is None:
+        return float('inf')
+
+    min_value = float('inf')
+    stack = [root]
+    while stack:
+        curr = stack.pop()
+        if curr.val < min_value:
+            min_value = curr.val
+
+        for child in (curr.right, curr.left):
+            if child is not None:
+                stack.append(child)
+    return min_value
+
+
 def tree_min(root):
     """Return the minimum value of a binary tree.
     >>> tree_min(root=Node(5, Node(10), Node(3)))
